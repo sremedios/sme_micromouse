9e7e34887428ddc6a2abbb52421dd1121cdcf641
sremedios <sam.remedios@gmail.com>
2018-10-12 10:54:18 -0500

    initial maze printing representation for debugging

---
+++ main.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#define MAZE_SIZE 25
+
+// ********** EMPTY INTERNAL MAZE REPRESENTATION ********** //
+int goal_coords(int i, int j, int maze_size) {
+    int center = (maze_size - 1) / 2; // lesser center
+
+    return center - 1 < i && i < center + 2 && 
+            center - 1 < j && j < center + 2;
+}
+
+void print_board(int maze[MAZE_SIZE][MAZE_SIZE]) {
+    for (int row = 0; row < MAZE_SIZE; ++row) {
+        for (int col = 0; col < MAZE_SIZE; ++col) {
+            printf("%d", maze[row][col]);
+        }
+        printf("\n");
+    }
+}
+
+void print_boards(int a[MAZE_SIZE][MAZE_SIZE], int b[MAZE_SIZE][MAZE_SIZE]) {
+    // Displays mazes side-by-size with a 5 space gap
+    for (int row = 0; row < MAZE_SIZE; ++row) {
+        for (int col = 0; col < MAZE_SIZE + MAZE_SIZE + 5; ++col) {
+            if (col < MAZE_SIZE) {
+                printf("%d", a[row][col]);
+            } else if (col > MAZE_SIZE && col <= (MAZE_SIZE + 5)){
+                printf(" ");
+            } else {
+                printf("%d", b[row][col-MAZE_SIZE-5]);
+            }
+        }
+        printf("\n");
+    }
+}
+
+void init_maze(int maze[MAZE_SIZE][MAZE_SIZE]) {
+    for (int i = 0; i < MAZE_SIZE; ++i) {
+        for (int j = 0; j < MAZE_SIZE; ++j) {
+            if (i == 0 || i == MAZE_SIZE - 1 || j == 0 || j == MAZE_SIZE -1) {
+                maze[j][i] = 1;
+            } else if (goal_coords(i, j, MAZE_SIZE)) {
+                maze[j][i] = 7;
+            } else {
+                maze[j][i] = 0;
+            }
+        }
+    }
+}
+
+// ********** MAZE ********** //
+
+int main() {
+    int internal_maze[MAZE_SIZE][MAZE_SIZE];
+
+    int test_maze[MAZE_SIZE][MAZE_SIZE] = {
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    }; 
+
+    init_maze(internal_maze);
+    //print_board(internal_maze);
+
+    //print_board(test_maze);
+    
+    print_boards(internal_maze, test_maze);
+    return 0;
+}
